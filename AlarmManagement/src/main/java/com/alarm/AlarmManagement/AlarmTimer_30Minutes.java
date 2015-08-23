fa0c623e3d35ac4f4e1bb867d63d37384953fc19
suleymandiker <suleyman.diker@gmail.com>
2015-08-23 19:16:34 +0300

    30 dakika periyodunda çalışan class eklendi

---
+++ AlarmManagement/src/main/java/com/alarm/AlarmManagement/AlarmTimer_30Minutes.java
@@ -0,0 +1,14 @@
+package com.alarm.AlarmManagement;
+
+import java.util.TimerTask;
+
+public class AlarmTimer_30Minutes extends TimerTask {
+
+	@Override
+	public void run() {
+		
+		System.out.println("30 dakika");
+		
+	}
+
+}
